e7f147d4f5fe7eae59ca84e06e59e38a1d8350c2
Arian27Kapllanaj <arian27kapllanaj@gmail.com>
2020-10-10 13:40:59 +0200

    return the sum of 2 numbers in c sharp

---
+++ Return the Sum of Two Numbers/C#/SumOfTwoNumbers/SumOfTwoNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static int Sum(int a, int b)
+            {
+                return a + b;
+            }
+        }
+
+     }
+
+}
